ec44ab8a58a7d958b9e1333bf6cb56a8ff1a004b
Armaan <armaanzeyad@gmail.com>
2025-05-28 02:05:53 +0530

    Initial Commit

---
+++ firstArProjectTests/firstArProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  firstArProjectTests.swift
+//  firstArProjectTests
+//
+//  Created by Armaan Zeyad on 28/05/25.
+//
+
+import Testing
+@testable import firstArProject
+
+struct firstArProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
